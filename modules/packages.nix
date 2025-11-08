f8bd42c9e7d7eaaef7d4390709e1a93ad2312720
João Víctor Santos Silva <joao.victor.ss.dev@gmail.com>
2025-11-08 13:37:46 -0300

    changing packages.nix

--- modules/packages.nix
+++ modules/packages.nix
@@ -9,7 +9,7 @@ with pkgs;
 
 let
   basePackages = [
-
+    # Build essentials
     efibootmgr
     gcc
     gnumake
@@ -20,11 +20,13 @@ let
     tree-sitter
     nodejs_22
 
+    # Fonts
     nerd-fonts.jetbrains-mono
     bibata-cursors
-
     font-awesome
     roboto
+
+    # LSP servers
     markdown-oxide
     mpls
     deno
@@ -36,6 +38,8 @@ let
     nil
     nixfmt-rfc-style
     nixpkgs-fmt
+
+    # CLI tools
     lazygit
     fzf
     fd
@@ -50,73 +54,76 @@ let
     bat
     ffmpeg
 
-    yazi
-    mpc
+    # Media
+    mpc-cli
     mpd
     mpd-mpris
     playerctl
     rmpc
+
+    # Wayland tools
     hypridle
     wleave
-    p7zip
-
     gtklock
+    wpaperd
+    gammastep
+
+    # System utilities
+    p7zip
     btop
+    brightnessctl
+    appimage-run
+    nh
+    grim
+    slurp
+    wl-clipboard
+    cliphist
+
+    # GUI applications
     keepassxc
     anydesk
     lmstudio
     qbittorrent
     mpv
-    gammastep
-    wpaperd
     imv
 
+    # Libraries
     libgcc
     lxqt.lxqt-policykit
     libnotify
-    ripgrep
-    bat
-    brightnessctl
 
-    appimage-run
-    playerctl
-    nh
-
-    grim
-    slurp
-    waybar
-    fuzzel
-    mako
-    wl-clipboard
-    pavucontrol
-    libvirt
-    qemu
-    virt-viewer
-    virt-manager
-    spice
-    spice-gtk
-    spice-protocol
-    OVMF
-    zed-editor
-    cliphist
-    zed-editor
-    fastfetch
+    # File manager
     xfce.thunar
     xfce.thunar-archive-plugin
     xfce.thunar-media-tags-plugin
     archiver
+    gvfs
+
+    # Polkit
     polkit
     mate.mate-polkit
-    gvfs
+
+    # Network
     networkmanagerapplet
 
-    libsForQt5.qt5.qtgraphicaleffectsr
+    # Qt
+    libsForQt5.qt5.qtgraphicaleffects
 
+    # Wayland bar/menu
+    waybar
+    fuzzel
+    mako
+    pavucontrol
+
+    # Editors
+    zed-editor
+
+    # Custom packages from inputs
     zen-browser.packages.${pkgs.system}.default
     helix.packages.${pkgs.system}.default
   ];
 in
 basePackages
 ++ pkgs.lib.optionals withGUI [
-  # GUI tools
+  # Additional GUI tools if needed
 ]
