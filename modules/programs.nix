7ad107cf89e34762394e11fe5b3d1a2d27e47b5b
João Víctor Santos Silva <joao.victor.ss.dev@gmail.com>
2025-11-08 13:46:36 -0300

    fixing programs.nix

--- modules/programs.nix
+++ modules/programs.nix
@@ -1,129 +1,27 @@
 {
   pkgs,
-  withGUI,
-  helix,
-  zen-browser,
+  config,
+  lib,
+  ...
 }:
 
-with pkgs;
+{
+  programs = {
+    git = {
+      enable = true;
+      lfs.enable = true;
+    };
 
-let
-  basePackages = [
-    # Build essentials
-    efibootmgr
-    gcc
-    gnumake
-    cargo
-    rustc
-    wget
-    unzip
-    tree-sitter
-    nodejs_22
+    direnv = {
+      enable = true;
+      nix-direnv.enable = true;
+    };
+  };
 
-    # Fonts
-    nerd-fonts.jetbrains-mono
-    bibata-cursors
-    font-awesome
-    roboto
-
-    # LSP servers
-    markdown-oxide
-    mpls
-    deno
-    zig
-    zls
-    go
-    gopls
-    nixd
-    nil
-    nixfmt-rfc-style
-    nixpkgs-fmt
-
-    # CLI tools
-    lazygit
-    fzf
-    fd
-    ncdu
-    ripgrep
-    fastfetch
-    nixos-shell
-    nix-your-shell
-    yazi
-    yaziPlugins.nord
-    yaziPlugins.starship
-    bat
-    ffmpeg
-
-    # Media
-    mpc-cli
-    mpd
-    mpd-mpris
-    playerctl
-    rmpc
-
-    # Wayland tools
-    hypridle
-    wleave
-    gtklock
-    wpaperd
-    gammastep
-
-    # System utilities
-    p7zip
-    btop
-    brightnessctl
-    appimage-run
-    nh
-    grim
-    slurp
-    wl-clipboard
-    cliphist
-
-    # GUI applications
-    keepassxc
-    anydesk
-    lmstudio
-    qbittorrent
-    mpv
-    imv
-
-    # Libraries
-    libgcc
-    lxqt.lxqt-policykit
-    libnotify
-
-    # File manager
-    xfce.thunar
-    xfce.thunar-archive-plugin
-    xfce.thunar-media-tags-plugin
-    archiver
-    gvfs
-
-    # Polkit
-    polkit
-    mate.mate-polkit
-
-    # Network
-    networkmanagerapplet
-
-    # Qt
-    libsForQt5.qt5.qtgraphicaleffects
-
-    # Wayland bar/menu
-    waybar
-    fuzzel
-    mako
-    pavucontrol
-
-    # Editors
-    zed-editor
-
-    # Custom packages from inputs
-    zen-browser.packages.${pkgs.system}.default
-    helix.packages.${pkgs.system}.default
-  ];
-in
-basePackages
-++ pkgs.lib.optionals withGUI [
-  # Additional GUI tools if needed
-]
+  xdg.configFile."direnv/direnv.toml".text = ''
+    [global]
+    hide_env_diff = true
+    warn_timeout = 0
+    log_filter="^$"
+  '';
+}
