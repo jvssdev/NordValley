6e0ddf9ed4490d6a77d88d59a538c8d6133ded7f
João Víctor Santos Silva <joao.victor.ss.dev@gmail.com>
2025-11-10 08:46:27 -0300

    match bracket fix

--- flake.nix
+++ flake.nix
@@ -11,170 +11,169 @@
     nur.url = "github:nix-community/NUR";
     mango.url = "github:DreamMaoMao/mango";
     quickshell.url = "git+https://git.outfoxxed.me/outfoxxed/quickshell";
+  };
+  outputs =
+    inputs@{
+      nixpkgs,
+      home-manager,
+      nur,
+      mango,
+      ...
+    }:
+    let
+      userInfo = {
+        userName = "joaov";
+        fullName = "João Víctor Santos Silva";
+        userEmail = "joao.victor.ss.dev@gmail.com";
+      };
+      pkgs = import nixpkgs {
+        system = "x86_64-linux";
+        config.allowUnfree = true;
+      };
+      system = pkgs.stdenv.hostPlatform.system;
 
-    outputs =
-      inputs@{
-        nixpkgs,
-        home-manager,
-        nur,
-        mango,
-        ...
-      }:
-      let
-        userInfo = {
-          userName = "joaov";
-          fullName = "João Víctor Santos Silva";
-          userEmail = "joao.victor.ss.dev@gmail.com";
-        };
+      defaults = {
+        withGUI = true;
+        homeDir = "/home/${userInfo.userName}";
+      };
+    in
+    {
+      # RiverWM configuration
+      nixosConfigurations.river = nixpkgs.lib.nixosSystem {
+        inherit system;
+        specialArgs =
+          inputs
+          // userInfo
+          // {
+            withGUI = defaults.withGUI;
+            homeDir = defaults.homeDir;
+            isRiver = true;
+            isMango = false;
+          };
+        modules = [
+          (
+            { config, pkgs, ... }:
+            {
+              nixpkgs.config.allowUnfree = true;
+            }
+          )
+
+          ./hosts/ashes/configuration.nix
+          ./hosts/ashes/hardware-configuration.nix
+          ./modules/path.nix
+          ./modules/services.nix
+          ./modules/elevated-packages.nix
+          ./modules/intel-drivers.nix
+          ./modules/power-management.nix
+          ./modules/river.nix
+
+          home-manager.nixosModules.home-manager
+          {
+            home-manager = {
+              useGlobalPkgs = true;
+              useUserPackages = true;
+              users.${userInfo.userName} = import ./modules/home.nix;
+              extraSpecialArgs = {
+                inherit (inputs)
+                  helix
+                  zen-browser
+                  helium-browser
+                  quickshell
+                  ;
+                inherit (userInfo) userName userEmail fullName;
+                inherit (defaults) withGUI homeDir;
+                isRiver = true;
+                isMango = false;
+              };
+            };
+          }
+        ];
+      };
+
+      # MangoWC
+      nixosConfigurations.mangowc = nixpkgs.lib.nixosSystem {
+        inherit system;
+        specialArgs =
+          inputs
+          // userInfo
+          // {
+            withGUI = defaults.withGUI;
+            homeDir = defaults.homeDir;
+            isRiver = false;
+            isMango = true;
+          };
+        modules = [
+          (
+            { config, pkgs, ... }:
+            {
+              nixpkgs.config.allowUnfree = true;
+            }
+          )
+
+          ./hosts/ashes/configuration.nix
+          ./hosts/ashes/hardware-configuration.nix
+          ./modules/path.nix
+          ./modules/services.nix
+          ./modules/elevated-packages.nix
+          ./modules/power-management.nix
+          ./modules/intel-drivers.nix
+          ./modules/mango.nix
+
+          # Enable mango at NixOS level
+          mango.nixosModules.mango
+          {
+            programs.mango.enable = true;
+          }
+
+          { nixpkgs.overlays = [ nur.overlays.default ]; }
+
+          home-manager.nixosModules.home-manager
+          {
+            home-manager = {
+              useGlobalPkgs = true;
+              useUserPackages = true;
+              backupFileExtension = "backup";
+              users.${userInfo.userName} = import ./modules/home.nix;
+              extraSpecialArgs = {
+                inherit (inputs)
+                  helix
+                  zen-browser
+                  helium-browser
+                  mango
+                  quickshell
+                  ;
+                inherit (userInfo) userName userEmail fullName;
+                inherit (defaults) withGUI homeDir;
+                isRiver = false;
+                isMango = true;
+              };
+            };
+          }
+        ];
+      };
+
+      homeConfigurations.universal = home-manager.lib.homeManagerConfiguration {
         pkgs = import nixpkgs {
           system = "x86_64-linux";
           config.allowUnfree = true;
         };
-        system = pkgs.stdenv.hostPlatform.system;
-
-        defaults = {
-          withGUI = true;
-          homeDir = "/home/${userInfo.userName}";
-        };
-      in
-      {
-        # RiverWM configuration
-        nixosConfigurations.river = nixpkgs.lib.nixosSystem {
-          inherit system;
-          specialArgs =
-            inputs
-            // userInfo
-            // {
-              withGUI = defaults.withGUI;
-              homeDir = defaults.homeDir;
-              isRiver = true;
-              isMango = false;
-            };
-          modules = [
-            (
-              { config, pkgs, ... }:
-              {
-                nixpkgs.config.allowUnfree = true;
-              }
-            )
-
-            ./hosts/ashes/configuration.nix
-            ./hosts/ashes/hardware-configuration.nix
-            ./modules/path.nix
-            ./modules/services.nix
-            ./modules/elevated-packages.nix
-            ./modules/intel-drivers.nix
-            ./modules/power-management.nix
-            ./modules/river.nix
-
-            home-manager.nixosModules.home-manager
-            {
-              home-manager = {
-                useGlobalPkgs = true;
-                useUserPackages = true;
-                users.${userInfo.userName} = import ./modules/home.nix;
-                extraSpecialArgs = {
-                  inherit (inputs)
-                    helix
-                    zen-browser
-                    helium-browser
-                    quickshell
-                    ;
-                  inherit (userInfo) userName userEmail fullName;
-                  inherit (defaults) withGUI homeDir;
-                  isRiver = true;
-                  isMango = false;
-                };
-              };
-            }
-          ];
-        };
-
-        # MangoWC
-        nixosConfigurations.mangowc = nixpkgs.lib.nixosSystem {
-          inherit system;
-          specialArgs =
-            inputs
-            // userInfo
-            // {
-              withGUI = defaults.withGUI;
-              homeDir = defaults.homeDir;
-              isRiver = false;
-              isMango = true;
-            };
-          modules = [
-            (
-              { config, pkgs, ... }:
-              {
-                nixpkgs.config.allowUnfree = true;
-              }
-            )
-
-            ./hosts/ashes/configuration.nix
-            ./hosts/ashes/hardware-configuration.nix
-            ./modules/path.nix
-            ./modules/services.nix
-            ./modules/elevated-packages.nix
-            ./modules/power-management.nix
-            ./modules/intel-drivers.nix
-            ./modules/mango.nix
-
-            # Enable mango at NixOS level
-            mango.nixosModules.mango
-            {
-              programs.mango.enable = true;
-            }
-
-            { nixpkgs.overlays = [ nur.overlays.default ]; }
-
-            home-manager.nixosModules.home-manager
-            {
-              home-manager = {
-                useGlobalPkgs = true;
-                useUserPackages = true;
-                backupFileExtension = "backup";
-                users.${userInfo.userName} = import ./modules/home.nix;
-                extraSpecialArgs = {
-                  inherit (inputs)
-                    helix
-                    zen-browser
-                    helium-browser
-                    mango
-                    quickshell
-                    ;
-                  inherit (userInfo) userName userEmail fullName;
-                  inherit (defaults) withGUI homeDir;
-                  isRiver = false;
-                  isMango = true;
-                };
-              };
-            }
-          ];
-        };
-
-        homeConfigurations.universal = home-manager.lib.homeManagerConfiguration {
-          pkgs = import nixpkgs {
-            system = "x86_64-linux";
-            config.allowUnfree = true;
-          };
-          extraSpecialArgs = {
-            withGUI = defaults.withGUI;
-            homeDir = defaults.homeDir;
-            helix = inputs.helix;
-            quickshell = inputs.quickshell;
-            zen-browser = inputs.zen-browser;
-            helium-browser = inputs.helium-browser;
-            isRiver = true;
-            isMango = false;
-          }
-          // userInfo;
-          modules = [ ./modules/home.nix ];
-        };
-
-        nixosConfigurations.iso = nixpkgs.lib.nixosSystem {
-          inherit system;
-          modules = [ ./hosts/iso/configuration.nix ];
-        };
+        extraSpecialArgs = {
+          withGUI = defaults.withGUI;
+          homeDir = defaults.homeDir;
+          helix = inputs.helix;
+          quickshell = inputs.quickshell;
+          zen-browser = inputs.zen-browser;
+          helium-browser = inputs.helium-browser;
+          isRiver = true;
+          isMango = false;
+        }
+        // userInfo;
+        modules = [ ./modules/home.nix ];
       };
-  };
+
+      nixosConfigurations.iso = nixpkgs.lib.nixosSystem {
+        inherit system;
+        modules = [ ./hosts/iso/configuration.nix ];
+      };
+    };
 }
