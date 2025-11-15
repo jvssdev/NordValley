a572b8d2aca52844cdb21cc5bfdafcc55264d808
João Víctor Santos Silva <joao.victor.ss.dev@gmail.com>
2025-11-15 09:08:27 -0300

    mako config fix pt.2

--- home/mako/default.nix
+++ home/mako/default.nix
@@ -4,7 +4,9 @@
   pkgs,
   ...
 }:
-
+let
+  c = config.colorScheme.palette;
+in
 {
   services.mako = {
     enable = true;
@@ -15,9 +17,20 @@
     defaultTimeout = 5000;
     margin = "8";
 
-    settings = ''
-      [hidden]
-      format=(and %h more)
-    '';
+    settings = {
+      default-timeout = "5000";
+      format = "<b>%a ⏵</b> %s\\n%b";
+      height = "110";
+      width = "300";
+      sort = "-time";
+      layer = "overlay";
+      margin = "5";
+      padding = "0,5,10";
+      border-size = "2";
+      border-color = "#${c.base0D}";
+      icons = "true";
+      max-icon-size = "64";
+      ignore-timeout = "true";
+    };
   };
 }
