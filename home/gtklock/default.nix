aa387302fd0d7b5beff5ca634ee2a70f79b4342c
João Víctor Santos Silva <joao.victor.ss.dev@gmail.com>
2025-11-15 07:55:19 -0300

    gtk config fix

--- home/gtklock/default.nix
+++ home/gtklock/default.nix
@@ -1,52 +1,50 @@
-{ config, pkgs, ... }:
-
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}:
 let
   c = config.stylix.colors;
+
+  gtklockConfig = pkgs.writeText "gtklock-config" ''
+    [main]
+    gtk-theme=Nordic
+    idle-hide=true
+    start-hidden=true
+    modules=userinfo-module
+
+    [userinfo]
+    round-image=true
+    horizontal-layout=false
+    under-clock=true
+  '';
+
+  gtklockStyle = pkgs.writeText "gtklock-style.css" ''
+    window {
+      background-color: #${c.base00};
+      background-image: url("${config.stylix.image}");
+      background-size: cover;
+      background-repeat: no-repeat;
+      background-position: center;
+    }
+    #clock-label {
+      color: #${c.base06};
+    }
+    #input-label {
+      font-size: 0;
+    }
+    window#eDP-1 #input-field {
+      margin-top: 32em;
+    }
+    #unlock-button {
+      color: rgba(${c.base00-rgb-r}, ${c.base00-rgb-g}, ${c.base00-rgb-b}, 0.87);
+    }
+  '';
 in
 {
-  programs.gtklock = {
-    enable = true;
-
-    config = {
-      main = {
-        gtk-theme = "Nordic";
-        idle-hide = true;
-        start-hidden = true;
-        modules = "userinfo-module";
-      };
-      userinfo = {
-        round-image = true;
-        horizontal-layout = false;
-        under-clock = true;
-      };
-    };
-
-    style = ''
-      window {
-        background-color: #${c.base00};
-        background-image: url("${config.stylix.image}");
-        background-size: cover;
-        background-repeat: no-repeat;
-        background-position: center;
-      }
-
-      #clock-label {
-        color: #${c.base06};
-      }
-
-      #input-label {
-        font-size: 0;
-      }
-
-      window#eDP-1 #input-field {
-        margin-top: 32em;
-      }
-
-      #unlock-button {
-        color: rgba(${c.base00_rgb_r}, ${c.base00_rgb_g}, ${c.base00_rgb_b}, 0.87);
-      }
-    '';
-  };
-
   home.packages = [ pkgs.gtklock ];
+
+  xdg.configFile."gtklock/config.ini".source = gtklockConfig;
+  xdg.configFile."gtklock/style.css".source = gtklockStyle;
 }
