9611130a27d6926054ac7d2f8028852402dfb51f
João Víctor Santos Silva <joao.victor.ss.dev@gmail.com>
2025-11-15 08:58:23 -0300

    zathura config fix

--- home/zathura/default.nix
+++ home/zathura/default.nix
@@ -1,36 +1,38 @@
 { config, ... }:
+let
+  c = config.colorScheme.palette;
+in
 {
   programs.zathura = {
     enable = true;
-    options = with config.colorScheme.palette {
-      
-      default-fg = "#${base05}";
-      default-bg = "#${base00}";
-      completion-bg = "#${base02}";
-      completion-fg = "#${base05}";
-      completion-highlight-bg = "#${base04}";
-      completion-highlight-fg = "#${base05}";
-      completion-group-bg = "#${base02}";
-      completion-group-fg = "#${base0E}";
-      statusbar-fg = "#${base05}";
-      statusbar-bg = "#${base02}";
-      notification-bg = "#${base02}";
-      notification-fg = "#${base05}";
-      notification-error-bg = "#${base02}";
-      notification-error-fg = "#${base08}";
-      notification-warning-bg = "#${base02}";
-      notification-warning-fg = "#${base09}";
-      inputbar-fg = "#${base05}";
-      inputbar-bg = "#${base02}";
-      index-fg = "#${base05}";
-      index-bg = "#${base00}";
-      index-active-fg = "#${base05}";
-      index-active-bg = "#${base02}";
-      render-loading-bg = "#${base00}";
-      render-loading-fg = "#${base05}";
-      recolor-lightcolor = "#${base00}";
-      recolor-darkcolor = "#${base05}";
-      recolor = "true";
+    options = {
+      default-fg = "#${c.base05}";
+      default-bg = "#${c.base00}";
+      completion-bg = "#${c.base02}";
+      completion-fg = "#${c.base05}";
+      completion-highlight-bg = "#${c.base04}";
+      completion-highlight-fg = "#${c.base05}";
+      completion-group-bg = "#${c.base02}";
+      completion-group-fg = "#${c.base0E}";
+      statusbar-fg = "#${c.base05}";
+      statusbar-bg = "#${c.base02}";
+      notification-bg = "#${c.base02}";
+      notification-fg = "#${c.base05}";
+      notification-error-bg = "#${c.base02}";
+      notification-error-fg = "#${c.base08}";
+      notification-warning-bg = "#${c.base02}";
+      notification-warning-fg = "#${c.base09}";
+      inputbar-fg = "#${c.base05}";
+      inputbar-bg = "#${c.base02}";
+      index-fg = "#${c.base05}";
+      index-bg = "#${c.base00}";
+      index-active-fg = "#${c.base05}";
+      index-active-bg = "#${c.base02}";
+      render-loading-bg = "#${c.base00}";
+      render-loading-fg = "#${c.base05}";
+      recolor-lightcolor = "#${c.base00}";
+      recolor-darkcolor = "#${c.base05}";
+      recolor = true;
     };
   };
 }
