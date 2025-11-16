a54d7c42ac4b4f33e1eaaa2b20999c7f3eb09944
João Víctor Santos Silva <joao.victor.ss.dev@gmail.com>
2025-11-16 18:37:00 -0300

    zsh plugins fix

--- home/zsh/default.nix
+++ home/zsh/default.nix
@@ -65,6 +65,7 @@ in
       (lib.mkOrder 1300 ''
         autoload -U select-word-style
         select-word-style bash
+
         export ZHM_STYLE_CURSOR_SELECT="fg:#${palette.base00},bg:#${palette.base08}"
         export ZHM_STYLE_CURSOR_INSERT="fg:#${palette.base00},bg:#${palette.base0B}"
         export ZHM_STYLE_OTHER_CURSOR_NORMAL="fg:#${palette.base00},bg:#${palette.base0C}"
@@ -72,6 +73,7 @@ in
         export ZHM_STYLE_OTHER_CURSOR_INSERT="fg:#${palette.base00},bg:#${palette.base0D}"
         export ZHM_STYLE_SELECTION="fg:#${palette.base07},bg:#${palette.base02}"
         export ZHM_CURSOR_INSERT='\e[0m\e[6 q\e]12;#${palette.base0B}\a'
+
         ZSH_AUTOSUGGEST_CLEAR_WIDGETS+=(
           zhm_history_prev zhm_history_next zhm_prompt_accept
           zhm_accept zhm_accept_or_insert_newline
@@ -82,15 +84,23 @@ in
         ZSH_AUTOSUGGEST_PARTIAL_ACCEPT_WIDGETS+=(
           zhm_move_next_word_start zhm_move_next_word_end
         )
+
         eval "$(${pkgs.fzf}/bin/fzf --zsh)"
-        zhm_wrap_widget fzf-completion zhm_fzf_completion
-        bindkey '^I' zhm_fzf_completion
+
         export FZF_DEFAULT_OPTS="
           --color=bg+:#${palette.base02},bg:#${palette.base00},spinner:#${palette.base04},hl:#${palette.base0D}
           --color=fg:#${palette.base05},header:#${palette.base0D},info:#${palette.base0C},pointer:#${palette.base04}
           --color=marker:#${palette.base0B},fg+:#${palette.base07},prompt:#${palette.base0C},hl+:#${palette.base0C}"
+
         eval "$(${pkgs.zoxide}/bin/zoxide init zsh)"
         eval "$(${pkgs.starship}/bin/starship init zsh)"
+      '')
+      (lib.mkOrder 1400 ''
+        # FZF integration with helix mode - runs after plugins are loaded
+        if (( $+functions[zhm_wrap_widget] )); then
+          zhm_wrap_widget fzf-completion zhm_fzf_completion
+          bindkey '^I' zhm_fzf_completion
+        fi
       '')
     ];
   };
