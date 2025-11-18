b1b17bf60f2a8d96d7387721394ef6a9e42d9bd9
João Víctor Santos Silva <joao.victor.ss.dev@gmail.com>
2025-11-18 16:53:39 -0300

    trying 2

--- modules/sddm-theme.nix
+++ modules/sddm-theme.nix
@@ -27,43 +27,47 @@ let
         fontSize = "12";
         blur = "true";
         blurRadius = "30";
-        enable-animations = true;
+        "enable-animations" = true;
       };
-      Theme.CursorTheme = "Bibata-Modern-Ice";
-      LoginScreen.LoginArea.PasswordInput = {
+      Theme = {
+        CursorTheme = "Bibata-Modern-Ice";
+      };
+      "LoginScreen.LoginArea.PasswordInput" = {
         width = 460;
         height = 60;
-        font-size = 22;
-        content-color = "#${colors.base05}";
-        background-color = "#${colors.base00}";
-        background-opacity = 0.7;
-        border-size = 2;
-        border-color = "#${colors.base0D}";
+        "font-size" = 22;
+        "content-color" = "#${colors.base05}";
+        "background-color" = "#${colors.base00}";
+        "background-opacity" = 0.7;
+        "border-size" = 2;
+        "border-color" = "#${colors.base0D}";
       };
-      LoginScreen.LoginArea.LoginButton = {
-        font-size = 22;
-        content-color = "#${colors.base05}";
-        background-color = "#${colors.base00}";
-        background-opacity = 0.7;
-        active-background-color = "#${colors.base0D}";
-        border-size = 2;
-        border-color = "#${colors.base0D}";
+      "LoginScreen.LoginArea.LoginButton" = {
+        "font-size" = 22;
+        "content-color" = "#${colors.base05}";
+        "background-color" = "#${colors.base00}";
+        "background-opacity" = 0.7;
+        "active-background-color" = "#${colors.base0D}";
+        "border-size" = 2;
+        "border-color" = "#${colors.base0D}";
       };
-      LoginScreen.LoginArea.Avatar = {
+      "LoginScreen.LoginArea.Avatar" = {
         shape = "circle";
-        active-size = 140;
+        "active-size" = 140;
       };
-      LoginScreen.MenuArea.Layout.position = "bottom-center";
+      "LoginScreen.MenuArea.Layout" = {
+        position = "bottom-center";
+      };
       LockScreen = {
         background = builtins.baseNameOf background-derivation;
         blur = 50;
       };
-      LockScreen.Clock = {
-        font-size = 92;
+      "LockScreen.Clock" = {
+        "font-size" = 92;
         color = "#${colors.base06}";
       };
-      LockScreen.Date = {
-        font-size = 32;
+      "LockScreen.Date" = {
+        "font-size" = 32;
         color = "#${colors.base0A}";
       };
     };
