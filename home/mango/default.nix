afe8904f5d9d1e5289fe94e28f689dc8d865625e
João Víctor Santos Silva <joao.victor.ss.dev@gmail.com>
2025-11-20 13:48:06 -0300

    mango config pt.2

--- home/mango/default.nix
+++ home/mango/default.nix
@@ -26,6 +26,9 @@
       # Screen share
       dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP=wlroots
       # systemctl --user set-environment XDG_CURRENT_DESKTOP=wlroots
+
+      #The next line of command is not necessary. It is only to avoid some situations where it cannot start automatically
+      /usr/lib/xdg-desktop-portal-wlr &
     '';
 
     settings = ''
