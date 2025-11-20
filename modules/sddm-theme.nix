d43d5821c57049dbbc56e81ccd91133663aadce1
João Víctor Santos Silva <joao.victor.ss.dev@gmail.com>
2025-11-20 06:01:17 -0300

    sddm-theme fix pt.4

--- modules/sddm-theme.nix
+++ modules/sddm-theme.nix
@@ -6,30 +6,26 @@
   nix-colors,
   ...
 }:
-
 let
-  wallpaper = ../Wallpapers/nord_valley.png;
-
   colors = nix-colors.colorSchemes.nord.palette;
-
+  wallpaper = ../Wallpapers/nord_valley.png;
+  background-derivation = pkgs.runCommand "nord_valley.png" { } ''
+    cp ${wallpaper} $out
+  '';
   silentTheme = silentSDDM.packages.${pkgs.stdenv.hostPlatform.system}.default.override {
-    extraBackgrounds = [ wallpaper ];
-
+    extraBackgrounds = [ background-derivation ];
     theme-overrides = {
       "General" = {
         enable-animations = true;
       };
-
       "LoginScreen" = {
-        background = wallpaper;
+        background = "nord_valley.png";
         blur = 30;
       };
-
       "LoginScreen.LoginArea" = {
         position = "center";
         margin = -1;
       };
-
       "LoginScreen.LoginArea.Avatar" = {
         shape = "circle";
         active-size = 140;
@@ -37,7 +33,6 @@ let
         active-border-size = 2;
         active-border-color = "#${colors.base0D}";
       };
-
       "LoginScreen.LoginArea.LoginButton" = {
         font-size = 22;
         icon-size = 30;
@@ -50,7 +45,6 @@ let
         border-size = 2;
         border-color = "#${colors.base0D}";
       };
-
       "LoginScreen.LoginArea.PasswordInput" = {
         width = 460;
         height = 60;
@@ -64,7 +58,6 @@ let
         border-color = "#${colors.base0D}";
         margin-top = 20;
       };
-
       "LoginScreen.LoginArea.Spinner" = {
         text = "Logging in";
         font-size = 36;
@@ -72,28 +65,23 @@ let
         color = "#${colors.base06}";
         spacing = 1;
       };
-
       "LoginScreen.LoginArea.Username" = {
         font-size = 40;
         color = "#${colors.base06}";
         margin = 5;
       };
-
       "LoginScreen.LoginArea.WarningMessage" = {
         font-size = 22;
         normal-color = "#${colors.base06}";
         warning-color = "#${colors.base0A}";
         error-color = "#${colors.base08}";
       };
-
       "LoginScreen.MenuArea.Buttons" = {
         size = 60;
       };
-
       "LoginScreen.MenuArea.Keyboard" = {
         display = false;
       };
-
       "LoginScreen.MenuArea.Layout" = {
         index = 2;
         position = "bottom-center";
@@ -106,7 +94,6 @@ let
         border-size = 2;
         border-color = "#${colors.base0D}";
       };
-
       "LoginScreen.MenuArea.Popups" = {
         max-height = 600;
         item-height = 60;
@@ -124,7 +111,6 @@ let
         border-color = "#${colors.base0D}";
         display-scrollbar = true;
       };
-
       "LoginScreen.MenuArea.Power" = {
         index = 0;
         popup-width = 200;
@@ -137,7 +123,6 @@ let
         border-size = 2;
         border-color = "#${colors.base0D}";
       };
-
       "LoginScreen.MenuArea.Session" = {
         index = 1;
         position = "bottom-center";
@@ -153,12 +138,10 @@ let
         border-size = 2;
         border-color = "#${colors.base0D}";
       };
-
       "LockScreen" = {
-        background = wallpaper;
+        background = "nord_valley.png";
         blur = 50;
       };
-
       "LockScreen.Clock" = {
         position = "center";
         align = "center";
@@ -166,7 +149,6 @@ let
         color = "#${colors.base06}";
         font-size = 92;
       };
-
       "LockScreen.Date" = {
         margin-top = 1;
         format = "dd/MM/yyyy";
@@ -174,7 +156,6 @@ let
         color = "#${colors.base0A}";
         font-size = 32;
       };
-
       "LockScreen.Message" = {
         text = "Pressione qualquer tecla";
         font-size = 32;
@@ -182,7 +163,6 @@ let
         icon-size = 44;
         paint-icon = true;
       };
-
       "Tooltips" = {
         enable = false;
       };
@@ -199,14 +179,12 @@ in
     kdePackages.qtwayland
     qt6.qtwayland
   ];
-
   services.displayManager.sddm = {
     enable = true;
     wayland.enable = true;
     package = pkgs.kdePackages.sddm;
     theme = "Silent";
   };
-
   environment.sessionVariables = {
     XCURSOR_THEME = "Bibata-Modern-Ice";
     XCURSOR_SIZE = "24";
