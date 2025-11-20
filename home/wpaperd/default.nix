a6c269cd92391dde3bd369304a5dc8a4bd3e618c
João Víctor Santos Silva <joao.victor.ss.dev@gmail.com>
2025-11-20 09:48:48 -0300

    wpaperd config pt.2

--- home/wpaperd/default.nix
+++ home/wpaperd/default.nix
@@ -1,21 +1,13 @@
 {
-  pkgs,
-  config,
-  specialArgs,
-  lib,
-  ...
-}:
-let
-  inherit (specialArgs)
-    homeDir
-    ;
-in
-
-{
   services.wpaperd = {
     enable = true;
     settings = {
       eDP-1 = {
+        path = ../../Wallpapers;
+        sorting = "random";
+        duration = "10m";
+      };
+      Virtual-1 = {
         path = ../../Wallpapers;
         sorting = "random";
         duration = "10m";
