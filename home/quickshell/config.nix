5261dc8d29e9dbd4d4bbf84ef58552eaba77fde6
João Víctor Santos Silva <joao.victor.ss.dev@gmail.com>
2025-11-29 17:23:02 -0300

    notif center pt.13

--- home/quickshell/config.nix
+++ home/quickshell/config.nix
@@ -8,41 +8,39 @@
 let
   p = config.colorScheme.palette;
 
-  themeQml = pkgs.writeText "Theme.qml" (
-    builtins.concatStringsSep "\n" [
-      "pragma Singleton"
-      "import QtQuick"
-      ""
-      "QtObject {"
-      "    readonly property color bg         : \"#${p.base00}\""
-      "    readonly property color bgAlt      : \"#${p.base01}\""
-      "    readonly property color bgLighter  : \"#${p.base02}\""
-      ""
-      "    readonly property color fg         : \"#${p.base05}\""
-      "    readonly property color fgMuted    : \"#${p.base04}\""
-      "    readonly property color fgSubtle   : \"#${p.base03}\""
-      ""
-      "    readonly property color red        : \"#${p.base08}\""
-      "    readonly property color green      : \"#${p.base0B}\""
-      "    readonly property color yellow     : \"#${p.base0A}\""
-      "    readonly property color blue       : \"#${p.base0D}\""
-      "    readonly property color magenta    : \"#${p.base0E}\""
-      "    readonly property color cyan       : \"#${p.base0C}\""
-      "    readonly property color orange     : \"#${p.base09}\""
-      ""
-      "    readonly property int radius       : 12"
-      "    readonly property int borderWidth  : 2"
-      "    readonly property int padding      : 14"
-      "    readonly property int spacing      : 10"
-      ""
-      "    readonly property font font        : Qt.font("
-      "        \"JetBrains Nerd Font Mono\","
-      "        14,"
-      "        Font.Medium"
-      "    )"
-      "}"
-    ]
-  );
+  themeQml = pkgs.writeText "Theme.qml" ''
+    pragma Singleton
+    import QtQuick
+
+    QtObject {
+        readonly property string bg         : "#${p.base00}"
+        readonly property string bgAlt      : "#${p.base01}"
+        readonly property string bgLighter  : "#${p.base02}"
+
+        readonly property string fg         : "#${p.base05}"
+        readonly property string fgMuted    : "#${p.base04}"
+        readonly property string fgSubtle   : "#${p.base03}"
+
+        readonly property string red        : "#${p.base08}"
+        readonly property string green      : "#${p.base0B}"
+        readonly property string yellow     : "#${p.base0A}"
+        readonly property string blue       : "#${p.base0D}"
+        readonly property string magenta    : "#${p.base0E}"
+        readonly property string cyan       : "#${p.base0C}"
+        readonly property string orange     : "#${p.base09}"
+
+        readonly property int radius       : 12
+        readonly property int borderWidth  : 2
+        readonly property int padding      : 14
+        readonly property int spacing      : 10
+
+        readonly property font font        : Qt.font({
+            family: "JetBrains Nerd Font Mono",
+            pixelSize: 14,
+            weight: Font.Medium
+        })
+    }
+  '';
 
 in
 {
