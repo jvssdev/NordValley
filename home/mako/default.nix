c671930af94060cdd8020a1068f424974b2caeb5
João Víctor Santos Silva <joao.victor.ss.dev@gmail.com>
2025-12-01 14:26:06 -0300

    mako pt.2

--- home/mako/default.nix
+++ home/mako/default.nix
@@ -9,27 +9,38 @@ let
 in
 {
   services.mako = {
-    backgroundColor = palette.base00;
-    borderColor = palette.base01;
-    textColor = palette.base04;
-    progressColor = "over ${palette.base02}";
-    font = "JetBrainsMono Nerd Font  12";
-    extraConfig = ''
+    enable = true;
+    settings = {
+      background-color = palette.base00;
+      border-color = palette.base01;
+      text-color = palette.base04;
+      progress-color = "over ${palette.base02}";
 
-      border-size=2
+      font = "JetBrainsMono Nerd Font 12";
+      border-size = 2;
+      border-radius = 8;
+      padding = "10";
+      margin = "8";
+      default-timeout = 5000;
+      ignore-timeout = 1;
 
-      ignore-timeout=1
-      default-timeout=5000
-      margin=8
+      "[hidden]" = {
+        format = "(and %h more)";
+        text-color = palette.base0D;
+        border-color = palette.base0D;
+      };
 
-      [hidden]
-      format=(and %h more)
-      text-color=${palette.base0D}
-      border-color=${palette.base0D}
+      "[urgency=high]" = {
+        background-color = palette.base0F;
+        border-color = palette.base08;
+        text-color = palette.base05;
+      };
 
-      [urgency=high]
-      background-color=${palette.base0F}
-      border-color=${palette.base08}
-    '';
+      "[urgency=critical]" = {
+        border-size = 4;
+        border-color = palette.base08;
+        default-timeout = 0;
+      };
+    };
   };
 }
