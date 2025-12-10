fc41c0b3794b1c96aa8f7c103954c2af3ccd7ec6
João Víctor Santos Silva <joao.victor.ss.dev@gmail.com>
2025-12-10 10:21:00 -0300

    mango config 3.5

--- home/mango/default.nix
+++ home/mango/default.nix
@@ -206,7 +206,6 @@ in
 
   xdg.portal = {
     enable = true;
-    wlr.enable = true;
     extraPortals = with pkgs; [
       xdg-desktop-portal-wlr
       xdg-desktop-portal-gtk
@@ -214,12 +213,10 @@ in
     config = {
       mango = {
         default = [ "gtk" ];
-        "org.freedesktop.impl.portal.Secret" = [ "gnome-keyring" ];
         "org.freedesktop.impl.portal.ScreenCast" = [ "wlr" ];
         "org.freedesktop.impl.portal.Screenshot" = [ "wlr" ];
         "org.freedesktop.impl.portal.Inhibit" = [ ];
       };
     };
   };
-
 }
