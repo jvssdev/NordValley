0bfedd5dc63cbe92a94d8e68ceefa8423296c44e
João Víctor Santos Silva <joao.victor.ss.dev@gmail.com>
2025-12-18 22:33:12 -0300

    kernel and virtualisation

--- hosts/ashes/configuration.nix
+++ hosts/ashes/configuration.nix
@@ -19,7 +19,6 @@
         "flakes"
       ];
       auto-optimise-store = true;
-      # Critical: Use XDG base directories for better flakes support
       use-xdg-base-directories = true;
     };
     gc = {
@@ -55,8 +54,9 @@
     algorithm = "zstd";
     memoryPercent = 50;
   };
-
   boot = {
+    kernelPackages = pkgs.linuxKernel.packages.linux_zen;
+
     loader = {
       systemd-boot = {
         enable = true;
@@ -68,22 +68,94 @@
     };
 
     kernelParams = [
-      # "lsm=landlock,lockdown,yama,integrity,apparmor,bpf,tomoyo,selinux"
       "quiet"
       "splash"
+      "loglevel=3"
+      "rd.systemd.show_status=auto"
+      "rd.udev.log_level=3"
+      "acpi_osi=Linux"
+      "pci=noaer"
+      "fbcon=nodefer"
+      "randomize_kstack_offset=on"
+      "vsyscall=none"
+      "slab_nomerge"
+      "module.sig_enforce=1"
+      "page_poison=1"
+      "page_alloc.shuffle=1"
+      "sysrq_always_enabled=0"
+      "rootflags=noatime"
+      "lsm=landlock,lockdown,yama,integrity,apparmor,bpf,tomoyo,selinux"
     ];
 
+    consoleLogLevel = 0;
+    initrd.verbose = false;
+
     kernel.sysctl = {
-      "kernel.dmesg_restrict" = 1;
+      "kernel.sysrq" = 0;
       "kernel.kptr_restrict" = 2;
+      "net.core.bpf_jit_enable" = false;
+      "kernel.ftrace_enabled" = false;
+      "kernel.dmesg_restrict" = 1;
+      "fs.protected_fifos" = 2;
+      "fs.protected_regular" = 2;
+      "fs.suid_dumpable" = 0;
+      "kernel.perf_event_paranoid" = 3;
+      "kernel.unprivileged_bpf_disabled" = 1;
       "net.ipv4.conf.all.rp_filter" = 1;
     };
+
+    blacklistedKernelModules = [
+      "af_802154"
+      "appletalk"
+      "atm"
+      "ax25"
+      "can"
+      "dccp"
+      "decnet"
+      "econet"
+      "ipx"
+      "n-hdlc"
+      "netrom"
+      "p8022"
+      "p8023"
+      "psnap"
+      "rds"
+      "rose"
+      "sctp"
+      "tipc"
+      "x25"
+      "adfs"
+      "affs"
+      "befs"
+      "bfs"
+      "cramfs"
+      "efs"
+      "erofs"
+      "exofs"
+      "freevxfs"
+      "gfs2"
+      "hfs"
+      "hfsplus"
+      "hpfs"
+      "jffs2"
+      "jfs"
+      "ksmbd"
+      "minix"
+      "nilfs2"
+      "omfs"
+      "qnx4"
+      "qnx6"
+      "sysv"
+      "udf"
+      "vivid"
+      "firewire-core"
+      "thunderbolt"
+    ];
   };
 
   security.pam.services.gtklock = {
     text = lib.readFile "${pkgs.gtklock}/etc/pam.d/gtklock";
   };
-
   powerManagement = {
     powertop.enable = true;
   };
--- modules/services.nix
+++ modules/services.nix
@@ -3,37 +3,41 @@
   userName,
   ...
 }:
-
 {
   virtualisation = {
     docker = {
       enable = true;
     };
-
     libvirtd = {
       enable = true;
+      onBoot = "ignore";
+      onShutdown = "shutdown";
       qemu = {
         package = pkgs.qemu_kvm;
         swtpm.enable = true;
+        runAsRoot = false;
+        verbatimConfig = ''
+          user = "${userName}"
+          group = "libvirtd"
+        '';
       };
     };
-
     spiceUSBRedirection.enable = true;
-
     waydroid.enable = true;
   };
 
+  systemd.services.libvirt-guests.enable = false;
+
   environment.systemPackages = with pkgs; [
     libvirt
-    qemu
+    qemu_kvm
     virt-viewer
     virt-manager
     spice
     spice-gtk
     spice-protocol
-    OVMF
-
-    # Waydroid dependencies
+    virglrenderer
+    mesa
     waydroid
     lxc
   ];
